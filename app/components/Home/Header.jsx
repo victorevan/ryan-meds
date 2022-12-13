6565666bba022b56d97887ded69d9392e73a938f
Victor Evangelista <victorevangelista@protonmail.com>
2022-12-13 12:37:53 -0800

    use next link on route change

--- app/components/Home/Header.jsx
+++ app/components/Home/Header.jsx
@@ -79,7 +79,7 @@ function MobileNavigation() {
             as="div"
             className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
           >
-            <MobileNavLink isA href="/medical-provider">Provider Sign in</MobileNavLink>
+            <MobileNavLink isA={false} href="/medical-provider">Provider Sign in</MobileNavLink>
           </Popover.Panel>
         </Transition.Child>
       </Transition.Root>
